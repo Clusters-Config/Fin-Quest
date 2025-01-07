feac145507f9ad81d6090ada65d0f5eb203c09d7
DineshG3778 <hsenid508@gmail.com>
2025-01-07 13:35:13 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import './App.css'
-// import Learning_paths from "./pages/Learning_paths"
-// import Ai_Driven from './pages/Ai-Driven'
-// import Communitys from './pages/Communitys'
-// import Facilitators from './pages/Facilitators'
-// import Game from './pages/Game'
-// import Homepage from './pages/Homepage'
-// import Login from './pages/Login-signup'
-// import Simulations from './pages/Simulations'
 import Learning_paths from "./pages/Learning_paths"
+
 
 
 const App = () => {
@@ -16,15 +9,6 @@ const App = () => {
     <>
     <div>
        <Learning_paths/>
-       
-      {/* <Ai_Driven/>
-      <Communitys/>
-      <Facilitators/>
-      <Game/> 
-      <Homepage/> */}
-
-      {/* <Login/> */}
-      {/* <Simulations/> */}
     </div>
     </>
   )
