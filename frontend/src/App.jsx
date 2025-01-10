71003c297949f035f433f53f220046c0a909936b
gowri1323 <gowri13905@gmail.com>
2025-01-10 12:08:35 +0530

    Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import Learning_paths from "./pages/Learning_paths"
 import Homepage from './pages/Homepage'
 import Game from "./pages/Game"
-
-
+import Login from "./pages/Login-signup"
+import Simulations from './pages/Simulations'
 const App = () => {
   return (
     <>
     <div>
        {/* <Learning_paths/> */}
        {/* <Homepage/> */}
-       <Game/>
+       {/* <Game/> */}
+       {/* <Login/> */}
+       <Simulations/>
     </div>
     </>
   )
