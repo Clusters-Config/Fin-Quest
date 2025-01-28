fee3fa54972e7de3418bde2710f3008c43b1060f
badri-2005 <badrinarayananramkumar@gmail.com>
2025-01-28 14:07:49 +0530

    Add community model to backend with schema definition

---
+++ backend/src/models/community.models.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const mongo = mongoose.Schema;
+
+const community = new mongo({
+    id : Number , 
+    name : String,
+    domain : String,
+    image : String,
+
+
+})
+
+const domain = mongoose.model('Domain',community);
+
+export default domain;
+
