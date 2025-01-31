a66d25439791a69a0d324a30156ddcdd36e511c6
DineshG3778 <hsenid508@gmail.com>
2025-01-31 11:19:05 +0530

    refactor: Modified imports in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,9 @@
 import express from "express";
-
-const app = express();
 import DbConnect from "./db/index.js";
+const app = express();
 
 DbConnect().then(() =>{
-    app.listen(process.env.PORT || 3008 , () =>{
+    app.listen(process.env.PORT || 3000 , () =>{
         console.log("Connected DB");
 
     } )
