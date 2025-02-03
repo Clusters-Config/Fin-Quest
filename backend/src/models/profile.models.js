fc166c2d62e7e74e0496089a126440b47f7d4c6a
badri-2005 <badrinarayananramkumar@gmail.com>
2025-02-03 21:58:13 +0530

    feat: Add profile schema with validation for user details

--- backend/src/models/profile.models.js
+++ backend/src/models/profile.models.js
@@ -1,1 +1,32 @@
- 
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const profile = new Schema({
+    firstname:{
+        type:String,
+        required:true,
+    },
+
+    lastname:{
+        type:String,
+        required:true,
+    },
+    dob:{
+        type:Date,
+        required:true,
+    },
+    phone:{
+        type:Number,
+        required:true,
+    },
+    hobbies:{
+        type:String,
+        required:true,
+    },
+},
+{
+    timestamps:true
+});
+
+const profileschema = mongoose.model("profile.models",profile);
+export{profileschema};
