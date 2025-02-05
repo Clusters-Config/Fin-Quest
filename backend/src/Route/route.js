460ba169a083fe18b4b642a2446bd6778c630cdf
badri-2005 <badrinarayananramkumar@gmail.com>
2025-02-05 21:07:11 +0530

    feat: add profile route to handle user profile requests

--- backend/src/Route/route.js
+++ backend/src/Route/route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { userlogin } from "../controllers/userlogin.js";
 import { Signup } from "../controllers/Signup.js";
+import { profile_user } from "../controllers/profile.controller.js"; 
 
 const router = Router();
 
 router.route("/login").post(userlogin);
 router.route("/signup").post(Signup);
+router.route("/profile").post(profile_user);
 
 
 export {router};
