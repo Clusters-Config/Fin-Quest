780364d00807113dae698261a073bfc17bea5a5b
gowri1323 <gowri13905@gmail.com>
2025-02-06 20:28:17 +0530

    Added changes in learning path profile page

--- frontend/src/learn_path_components/ProfilePage.jsx
+++ frontend/src/learn_path_components/ProfilePage.jsx
@@ -7,17 +7,19 @@ const ProfilePage = () => {
     dob: "2000-05-15",
     phone: "+1 234 567 890",
     achievements: [
-      "Completed Module 1: Basic Financial Concepts",
-      "Achieved 40% Progress in Module 2: Understanding Interest Rates",
+      "Completed Module 1: Basic Terminologies",
+      "Achieved 95% Progress in Module 2: Fundamentals of Accounting",
       "Won 'Student of the Month' award for December 2024",
     ],
   };
 
   // Learning modules progress
   const [modules, setModules] = useState([
-    { name: "Module 1: Basic Financial Concepts", progress: 100 },
-    { name: "Module 2: Understanding Interest Rates", progress: 40 },
-    { name: "Module 3: Investment Basics", progress: 20 },
+    { name: "Module 1: Basic Terminologies", progress: 100 },
+    { name: "Module 2: Fundamentals of Accounting", progress: 95 },
+    { name: "Module 3: Basic Financial Concepts", progress: 30 },
+    { name: "Module 4: Understanding Interest Rates", progress: 10 },
+    { name: "Module 5: Investment Basics", progress: 0 },
   ]);
 
   return (
