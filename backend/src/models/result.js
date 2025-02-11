e59aec7b36f5f00d35450af9b3f477c38e6911dc
badri-2005 <badrinarayananramkumar@gmail.com>
2025-02-11 21:03:49 +0530

    feat : added result schema

---
+++ backend/src/models/result.js
@@ -0,0 +1,100 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const resultSchema = new Schema({
+        email:{
+            type:String,
+            lowercase:true
+        },
+        module: [
+            {
+            mod1: 
+            [
+                {
+                    path1:
+                    {
+                        type: Number,
+                        required: true,
+                    },
+                    path2:
+                    {
+                        type: Number,
+                        required: true,
+                    }
+                }  
+            ],
+            mod2: 
+            [
+                {
+                    path1:
+                    {
+                        type: Number,
+                        required: true,
+                    },
+                    path2:
+                    {
+                        type: Number,
+                        required: true,
+                    }
+                }  
+            ],
+            mod3: 
+            [
+                {
+                    path1:
+                    {
+                        type: Number,
+                        required: true,
+                    },
+                    path2:
+                    {
+                        type: Number,
+                        required: true,
+                    }
+                }  
+            ],
+            mod4: 
+            [
+                {
+                    path1:
+                    {
+                        type: Number,
+                        required: true,
+                    },
+                    path2:
+                    {
+                        type: Number,
+                        required: true,
+                    }
+                }  
+            ],
+            mod5: 
+            [
+                {
+                    path1:
+                    {
+                        type: Number,
+                        required: true,
+                    },
+                    path2:
+                    {
+                        type: Number,
+                        required: true,
+                    },
+                    path3:
+                    {
+                        type: Number,
+                        required: true,
+                    }
+                }  
+            ],
+        }]
+    
+    },
+    {
+        timestamps:true
+});
+
+
+const ResultSchema = mongoose.model("Result",resultSchema);
+export{ResultSchema};
